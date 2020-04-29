967b10566266bca4682eac7d71ba440e15ab23c8
Achenson <ach.01.mail@gmail.com>
2020-04-29 11:12:15 +0200

    schema initial

---
+++ schema/schema.js
@@ -0,0 +1,45 @@
+const graphql = require("graphql");
+
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    score: { type: ScoreType, resolve(parent, args) {
+        
+        
+
+    } },
+  }),
+});
+
+const ScoreType = new GraphQLObjectType({
+  name: "Score",
+  fields: () => ({
+    id: { type: GraphQLID },
+    "5sec": { type: GraphQLList },
+    "30sec": { type: GraphQLList },
+    "1min": { type: GraphQLList },
+    "2min": { type: GraphQLList },
+    "5min": { type: GraphQLList },
+
+    user: { type: UserType, resolve(parent, args) {} },
+  }),
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
