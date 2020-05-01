b3b2db21eae5423ea4f2b0538f33c39b3a5f1929
Achenson <ach.01.mail@gmail.com>
2020-05-01 16:35:50 +0200

    upsert works2

--- schema/schema.js
+++ schema/schema.js
@@ -109,6 +109,7 @@ const Mutation = new GraphQLObjectType({
         };
 
         return Score.findOneAndUpdate({ userId: args.userId }, update, {
+          // to return updated object
           new: true,
           upsert: true, // Make this update into an upsert,
           useFindAndModify: false,
