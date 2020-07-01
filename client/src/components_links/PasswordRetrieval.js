b530c89c2330167e77231870812743c094e81976
achenson <45999022+Achenson@users.noreply.github.com>
2020-07-01 16:07:12 +0200

    PasswordRetrieval frontend & backend

--- client/src/components_links/PasswordRetrieval.js
+++ client/src/components_links/PasswordRetrieval.js
@@ -12,112 +12,69 @@ import { useMutation } from "@apollo/react-hooks";
 
 import { forgotPassword } from "../graphql/queries.js";
 
-function PasswordRetrieval({
-  logIn,
-  isNotificationNeeded,
-  showLoginError,
-
-  notification_false,
-  loginError_true,
-  loginError_false,
-  registerError_false,
-  loginErrorMessage,
-  setLoginErrorMessage,
-}) {
-  
-
+function PasswordRetrieval({}) {
   const [forgotPass] = useMutation(forgotPassword);
-
-  /*   const {loading, err, data} = useQuery(getUserByEmailQuery, {
-    variables: {email: "l@l.pl" }, });
-
-  if (data) {
-    console.log('dataaaa')
-    console.log(data)
-  } */
-
-  // reseting authState for Register, so auth notifications/warnings disappear
-  // when going back to Register
-  useEffect(() => {
-    // return () => {
-    registerError_false();
-    // };
-  }, [registerError_false]);
 
   // not {history}!!! because we are not destructuring here,
   // history is an object!
   let history = useHistory();
 
-  let [notification, setNotification] = useState(null);
 
+  let [errorNotification, setErrorNotification] = useState(null);
+  let [infoNotification, setInfoNotification] = useState(null);
 
+  useEffect(() => {
+    // after the component is unmounted!
+    return () => {
+      setErrorNotification(null);
+      setInfoNotification(null);
+    };
+  }, [setErrorNotification, setInfoNotification]);
 
   let [email, setEmail] = useState("");
   // let [password, setPassword] = useState("");
 
   function emailValidation() {
-    if (email=== "") {
+    if (email === "" || email.indexOf("@") === -1) {
       // setError("Email or password not provided");
-      setLoginErrorMessage("Email not provided");
-      loginError_true();
+      setErrorNotification("Invalid email");
       return;
     }
 
     forgotPass({
       variables: {
         email: email,
-        
       },
     }).then((res, err) => {
       if (err) {
         // setError("loginMut Error");
-        setLoginErrorMessage("loginMut Error");
-        loginError_true();
+        setErrorNotification("Server error - email not sent");
         return;
       }
 
-      if (res.data.login.token === "User does not exist!") {
-        // setError(`${res.data.login.token}`);
-        setLoginErrorMessage(`${res.data.login.token}`);
-        loginError_true();
-        return;
-      }
-
-      if (res.data.login.token === "Password is incorrect!") {
-        // setError(`${res.data.login.token}`);
-        setLoginErrorMessage(`${res.data.login.token}`);
-        loginError_true();
-        return;
-      }
-
-      console.log("loginMut res");
+      console.log("forgotPassword res");
       console.log(res);
 
-      loginError_false();
-      logIn({
-        authenticatedUserId: res.data.forgotPass.userId,
-        token: res.data.forgotPassword.token,
-      });
-      notification_false();
+      setErrorNotification(null);
+      setInfoNotification("Email successfully sent");
 
-      // history.push('/')
-      // no going back! not possible to go back to login when logged in
-      history.replace("/");
+      // history.replace("/");
     });
   }
 
   return (
     <div>
-      {isNotificationNeeded ? (
+      {errorNotification ? (
         <AuthNotification
-          notification={notification}
-          colorClass={"auth-notification-info"}
+          notification={errorNotification}
+          colorClass={"auth-notification-danger"}
         />
       ) : null}
-      {showLoginError ? (
+
+      {infoNotification ? (
         <AuthNotification
-          notification={loginErrorMessage}
-          colorClass={"auth-notification-danger"}
+          notification={infoNotification}
+          colorClass={"auth-notification-info"}
         />
       ) : null}
 
@@ -127,10 +84,11 @@ function PasswordRetrieval({
             <div className="title-auth-div">
               <h3 className="title title-auth">Password Retrieval</h3>
             </div>
-            
-            <p style={{color: "steelblue"}}>A password change link will be sent to the provided email address.</p>
 
-            
+            <p style={{ color: "steelblue" }}>
+              A password change link will be sent to the provided email address.
+            </p>
+
             <form className="form">
               {/* associating label with input without ID -> nesting */}
               <label className="label">
@@ -145,10 +103,6 @@ function PasswordRetrieval({
                 />
               </label>
               <br />
-            
-
-         
-             
 
               <button
                 className="btn btn-control btn-auth"
@@ -192,21 +146,14 @@ function PasswordRetrieval({
 
 const mapStateToProps = (state) => {
   return {
-    isNotificationNeeded: state.authState.isNotificationNeeded,
-    showLoginError: state.authState.showLoginError,
-    loginErrorMessage: state.authState.loginErrorMessage,
+    
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    logIn: (dataObj) => dispatch({ type: "LOG_IN", payload: dataObj }),
-    notification_false: () => dispatch({ type: "NOTIFICATION_FALSE" }),
-    loginError_true: () => dispatch({ type: "LOGIN_ERROR_TRUE" }),
-    loginError_false: () => dispatch({ type: "LOGIN_ERROR_FALSE" }),
-    registerError_false: () => dispatch({ type: "REGISTER_ERROR_FALSE" }),
-    setLoginErrorMessage: (error) =>
-      dispatch({ type: "SET_LOGIN_ERROR_MESSAGE", payload: error }),
+    
+    
   };
 };
 
