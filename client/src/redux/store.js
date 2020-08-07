789ac2549713a9876dc64ca99adc7544ee30e7bb
Achenson <ach.01.mail@gmail.com>
2020-08-07 15:04:09 +0200

    heroku: store.js redux commented out

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,6 @@
 // import { createStore } from "redux";
-import { createStore, applyMiddleware, compose } from "redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./rootReducer.js";
@@ -13,10 +14,12 @@ const store = createStore(
   rootReducer,
   initialState,
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  compose(
+
+  // compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
+
 );
 /* eslint-enable */
 
